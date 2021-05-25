0e1dd3822fbbcbb14cb878467b7e72ae11119ebf
Russell Holmes <team.holmes@gmail.com>
2021-05-25 23:44:56 +0100

    Converting to JEST

--- test/multiplyBy.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const multiplyBy = require('../code/multiplyBy');
-
-describe('task1', () => {
-  it('passes all the tests', () => {
-    assert.equal(multiplyBy(5)(10), 50);
-    assert.equal(multiplyBy(2)(3), 6);
-  });
-});
